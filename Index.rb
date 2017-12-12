2ac89d6aaccc05d37a95dff6400386469d651863
Jorge Díaz <joredudiaz@gmail.com>
2017-12-12 10:17:30 -0500

    UPDATE Readme.md Se cambia la forma de compilación de python

--- Index.rb
+++ Index.rb
@@ -11,35 +11,39 @@ config.readConfig()
 log = Log.new config.uploadPath
 
 if(Dir.exists?(config.pathInstall) and Dir.exists?(config.pathCopy))
-  backup = Backup.new(config.pathInstall, config.pathCopy)
+  begin
+    backup = Backup.new(config.pathInstall, config.pathCopy)
 
-  log.sendLog('Obtener elementos', 'Se obtienen los elementos a copiar')
-  filesReports = backup.getFiles("reportes")
-  filesReportsDOS = backup.getFiles("repodos")
-  filesDataBase = backup.getFiles("#{config.dataBaseFolder}\\datos")
+    log.sendLog('Obtener elementos', 'Se obtienen los elementos a copiar')
+    filesReports = backup.getFiles("reportes")
+    filesReportsDOS = backup.getFiles("repodos")
+    filesDataBase = backup.getFiles("#{config.dataBaseFolder}\\datos")
 
-  log.sendLog('Crear directorios', 'Se crean los directorios de copia')
-  backup.createDirectories()
+    log.sendLog('Crear directorios', 'Se crean los directorios de copia')
+    backup.createDirectories()
 
+    log.sendLog('Copiar elementos', 'Se copia los elementos en los directorios de copia')
+    backup.copyFilesToBackupDirectory(filesReports, "reportes")
+    backup.copyFilesToBackupDirectory(filesReportsDOS, "repodos")
+    backup.copyFilesToBackupDirectory(filesDataBase, "#{config.dataBaseFolder}\\datos", "DB")
 
-  log.sendLog('Copiar elementos', 'Se copia los elementos en los directorios de copia')
-  backup.copyFilesToBackupDirectory(filesReports, "reportes")
-  backup.copyFilesToBackupDirectory(filesReportsDOS, "repodos")
-  backup.copyFilesToBackupDirectory(filesDataBase, "#{config.dataBaseFolder}\\datos", "DB")
+    if(File.exists?("#{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.zip"))   # Delete .zip file if exists
+      backup.deleteCopyFile()
+    end
 
-  if(File.exists?("#{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.zip"))   # Delete .zip file if exists
-    backup.deleteCopyFile()
+    log.sendLog('Comprimir archivos', 'Se comprimen los elementos')
+    puts %x(rar a -r #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.rar #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")})
+
+    log.sendLog('Cargar archivo de copia', 'Se carga el archivo de copia generado')
+    upload = Upload.new
+    upload.uploadCopy(config.pathCopy, config.uploadPath)
+
+    log.sendLog('Eliminar copias locales', 'Se eliminan los directorios locales de copia')
+    backup.deleteCopyDirectory()
+  rescue Exception => e
+    puts e.message
+    puts %x(library/reminder.exe #{config.pathInstall} #{config.dataBaseFolder} "No se realizó la copia de seguridad")
   end
-
-  log.sendLog('Comprimir archivos', 'Se comprimen los elementos')
-  puts %x(rar a -r #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.rar #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")})
-
-  log.sendLog('Cargar archivo de copia', 'Se carga el archivo de copia generado')
-  upload = Upload.new
-  upload.uploadCopy(config.pathCopy, config.uploadPath)
-
-  log.sendLog('Eliminar copias locales', 'Se eliminan los directorios locales de copia')
-  backup.deleteCopyDirectory()
 else
   log.sendLog('Configuracion erronea', 'No existen los directorios configurados')
   puts "Error: directorios configurados no existen"
--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 # Requerimientos
-- ruby 2.3
+- ruby 2.3.3
+- python 3.6.2
 - RAR versión gratuita o superior
 
 ## Gemas
@@ -7,8 +8,17 @@
 - net-sftp
 - rest-client
 
-# Compilación
+## Package
+- pip
+- dbf
+- pyinstaller 3.3 [pip install --upgrade pyinstaller]
+
+# Compilación Ruby
 ocra Index.rb --verbose --add-all-core
+
+# Compilación python
+En usuario administrador
+pyinstaller -F reminder.py
 
 ## Instalación
 Debe agregarse en una variable de entorno de WINDOWS el PATH donde se encuentre el archivo rar.exe quien es el encargado de comprimir los archivos.
