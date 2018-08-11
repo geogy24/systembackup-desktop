4aff6d2ec48ff9dc1ea72de9140e11fbaba95c18
Jorge Díaz <joredudiaz@gmail.com>
2018-08-10 22:17:46 -0500

    UPDATE uncomment script

--- Index.rb
+++ Index.rb
@@ -9,17 +9,18 @@ config.readConfig()
 
 if(Dir.exists?(config.pathInstall) and Dir.exists?(config.pathCopy))
   begin
-    #backup = Backup.new(config.pathInstall, config.pathCopy, config.dataBaseFolder)
-    #backup.initializeDirectory()
-    #backup.makeCopy()
-
-    #puts "Comprimiendo copia"
-    #puts %x(rar a -r #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.rar #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")})
+    backup = Backup.new(config.pathInstall, config.pathCopy, config.dataBaseFolder)
+    backup.initializeDirectory()
+    backup.makeCopy()
+    
+    puts "Comprimiendo copia"
+    puts %x(rar a -r #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")}.rar #{config.pathCopy}\\#{DateTime.now.strftime("%Y_%m_%d")})
+    
     puts "Cargando.."
     upload = Upload.new(config.pathCopy, config.uploadPath)
     upload.upload()
 
-    #backup.deleteCopyDirectory()
+    backup.deleteCopyDirectory()
   rescue Exception => e
     puts "Error #{e.message}"
     #puts %x(library/reminder.exe #{config.pathInstall} #{config.dataBaseFolder} "No se realizó la copia de seguridad #{e.message}")
